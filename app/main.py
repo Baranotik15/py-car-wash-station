781e1daeb5f349c53c6f22a7f7319c389c86a834
RuslanTopal <baranotik15@gmail.com>
2024-12-30 02:45:28 +0000

    Solve

--- app/main.py
+++ app/main.py
@@ -1,8 +1,48 @@
 class Car:
-    # write your code here
-    pass
+    def __init__(self, comfort_class: int,
+                 clean_mark: int, brand: str) -> None:
+
+        self.brand = brand
+        self.comfort_class = comfort_class
+        self.clean_mark = clean_mark
 
 
 class CarWashStation:
-    # write your code here
-    pass
+    def __init__(self, distance_from_city_center: float, clean_power: int,
+                 average_rating: float, count_of_ratings: int) -> None :
+
+        self.distance_from_city_center = distance_from_city_center
+        self.clean_power = clean_power
+        self.average_rating = average_rating
+        self.count_of_ratings = count_of_ratings
+
+    def serve_cars(self, list_of_cars: list) -> float:
+        """Обслуживает список машин и возвращает общую стоимость мойки."""
+        total_income = 0
+        if isinstance(list_of_cars, Car):
+            return self.wash_single_car(list_of_cars)
+        for car in list_of_cars:
+            total_income += self.wash_single_car(car)
+        return total_income
+
+    def calculate_washing_price(self, car: Car) -> float:
+        """Рассчитывает стоимость мойки для конкретной машины."""
+        result = (car.comfort_class * (self.clean_power - car.clean_mark)
+                  * self.average_rating / self.distance_from_city_center)
+        return round(result, 1)
+
+    def wash_single_car(self, car: Car) -> float:
+        """Моет одну машину, если мощность станции больше
+         текущей чистоты машины."""
+        if self.clean_power > car.clean_mark:
+            result = self.calculate_washing_price(car)
+            car.clean_mark = self.clean_power
+            return result
+        return 0.0
+
+    def rate_service(self, rating: float) -> None:
+        """Обновляет средний рейтинг станции после новой оценки."""
+        self.average_rating = round((self.average_rating
+                                    * self.count_of_ratings + rating)
+                                    / (self.count_of_ratings + 1), 1)
+        self.count_of_ratings += 1
